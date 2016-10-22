ce3173788ce776ea447e6c593c40586b05ea72fd
hua3505 <959547664@qq.com>
2016-10-22 12:00:15 +0800

    add pacman

---
+++ Assets/PacMan.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacMan : MonoBehaviour {
+
+	public GameObject _camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotateCamera ();
+	}
+
+	void RotateCamera() {
+		
+	}
+}
