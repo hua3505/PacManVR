12f1bebd2ec666e5e0f16303c4139dceb335d195
hua3505 <959547664@qq.com>
2016-10-25 17:54:37 +0800

    add ghost navigation

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : MonoBehaviour {
+
+	private NavMeshAgent _agent;
+	private GameObject _pacMan;
+
+	// Use this for initialization
+	void Start () {
+		_agent = GetComponent<NavMeshAgent> ();
+		_pacMan = GameObject.FindGameObjectWithTag ("PacMan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_agent.SetDestination (_pacMan.transform.position);
+	}
+}
